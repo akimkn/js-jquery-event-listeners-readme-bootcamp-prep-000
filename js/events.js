7580f82be154850ddadea2bc3847727c26823ab3
Austin Kim <ask199416@gmail.com>
2017-10-20 06:29:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,8 +1,8 @@
 //define functions here
 function getIt() {
-  if $('p').on('click', function()) {
+  $('p').on('click', function() {
     alert('Hey!')
-  }
+  })
 }
 
 $(document).ready(function(){
